c6eabb522624cc35947836f76d72086e71e5eff3
Stephan Göppentin <dedda102@gmail.com>
2020-06-26 12:12:48 +0200

    remove some comments

--- static/js/base.js
+++ static/js/base.js
@@ -9,16 +9,11 @@ let wsConnection = function wsConnection() {
 
 let copyToClipboard = function copyToClipboard(text) {
     var el = document.createElement('textarea');
-    // Den zu kopierenden String dem Element zuweisen
     el.value = text;
-    // Element nicht editierbar setzen und aus dem Fenster schieben
     el.setAttribute('readonly', '');
     el.style = {position: 'absolute', left: '-9999px'};
     document.body.appendChild(el);
-    // Text innerhalb des Elements auswählen
     el.select();
-    // Ausgewählten Text in die Zwischenablage kopieren
     document.execCommand('copy');
-    // Temporäres Element löschen
     document.body.removeChild(el);
 }
